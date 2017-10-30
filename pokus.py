fde6dbd3ef3aaf111a25badd5a4e6d99a38255cc
ibisek <ibisek@protonmail.com>
2017-10-30 22:45:30 +0100

    wall + syslog

--- pokus.py
+++ pokus.py
@@ -2,6 +2,8 @@
 
 
 import sys
+import syslog
+import subprocess
 
 
 def printBatteryVoltage():
@@ -20,12 +22,49 @@ def printTimeOnBattery():
     print("printTimeOnBattery()")
 
 
+def printBatteryLow():
+    print("printBatteryLow()")
+
+
 def doShutdown(timeout=None):
     print("doShutdown(%s)" % timeout)
-    print("POWER CUT-OFF in (%s) seconds!" % timeout)
+
+    #TODO read-out timeout if None
+    #TODO initiate power-off countdown
+
+    message = "UPS POWER OFF in {} seconds!".format(timeout)
+    log(message)
+    wall(message)
 
 def cancelShutdown():
     print("cancelShutdown()")
+
+    #TODO cancel shutdown
+
+    message = "UPS power off cancelled"
+    log(message)
+    wall(message)
+
+
+def checkPowerStatus():
+    #TODO vycist z upsky
+    batteryVoltage = 3.78
+    onBattery = True
+    timeOnBattery = 45
+    batteryLow = False
+
+    message = "Status check: battery voltage: {:.2f}V; on battery: {}".format(batteryVoltage, onBattery)
+    if onBattery:
+        message = "{}; time on battery: {:}s; battery low: {}".format(message, timeOnBattery, batteryLow)
+
+    log(message)
+
+
+def log(message):
+    syslog.syslog(syslog.LOG_INFO, message)
+
+def wall(message):
+    subprocess.run(['/usr/bin/wall', message])
 
 
 def printHelp():
@@ -34,6 +73,7 @@ def printHelp():
     print('\tbatt\t\treturns battery voltage [V]')
     print('\tonbatt\t\treturns whether we run on battery (1) or on external power (0)')
     print('\ttime\t\treturns run time on battery in [s] or 0 if powered from external source')
+    print('\tbattlow\t\treturns (1) if battery voltage is too low, (0) otherwise')
     print('\thalt [t]\tinitiates UPS power off after an optionally defined timeout (default 30s)')
     print('\tcancel\t\tcancels UPS power off countdown')
 
@@ -49,6 +89,8 @@ def parseArguments():
             printVersion()
         elif cmd == 'time':
             printTimeOnBattery()
+        elif cmd == 'battlow':
+            printBatteryLow()
         elif cmd == 'halt':
             if len(sys.argv) == 3:
                 doShutdown(int(sys.argv[2]))
@@ -56,6 +98,8 @@ def parseArguments():
                 doShutdown()
         elif cmd == 'cancel':
             cancelShutdown()
+        elif cmd == 'check':
+            checkPowerStatus()
         else:
             printHelp()
     else:
