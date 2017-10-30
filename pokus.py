0fba96f6715ace200c6ff586c1ba2223765c1a4a
ibisek <ibisek@protonmail.com>
2017-10-30 21:56:16 +0100

    +cancel

--- pokus.py
+++ pokus.py
@@ -1,51 +1,65 @@
 #!/usr/bin/python3
 
+
 import sys
 
-def showBatteryVoltage():
+
+def printBatteryVoltage():
     print("BATTERY VOLTAGE")
 
 
-def showIfOnBattery():
+def printIfOnBattery():
     print("on battery? nevim :P");
 
 
-def showVersion():
-    print("showVersion()")
+def printVersion():
+    print("printVersion()")
 
 
-def showTimeOnBattery():
-    print("showTimeOnBattery()")
+def printTimeOnBattery():
+    print("printTimeOnBattery()")
 
 
 def doShutdown(timeout=None):
     print("doShutdown(%s)" % timeout)
     print("POWER CUT-OFF in (%s) seconds!" % timeout)
 
+def cancelShutdown():
+    print("cancelShutdown()")
+
+
+def printHelp():
+    print('## raspiUps ## control script\nusage: piUps.py [ver] [batt] [onbatt] [time] [halt [t]] [cancel]')
+    print('\tver\t\treturns UPS version in form of hw.fw')
+    print('\tbatt\t\treturns battery voltage [V]')
+    print('\tonbatt\t\treturns whether we run on battery (1) or on external power (0)')
+    print('\ttime\t\treturns run time on battery in [s] or 0 if powered from external source')
+    print('\thalt [t]\tinitiates UPS power off after an optionally defined timeout (default 30s)')
+    print('\tcancel\t\tcancels UPS power off countdown')
+
 
 def parseArguments():
     if len(sys.argv) > 1:
         cmd = str(sys.argv[1]).lower()
         if cmd == 'batt':
-            showBatteryVoltage()
+            printBatteryVoltage()
         elif cmd == 'onbatt':
-            showIfOnBattery()
+            printIfOnBattery()
         elif cmd == 'ver':
-            showVersion()
+            printVersion()
         elif cmd == 'time':
-            showTimeOnBattery()
+            printTimeOnBattery()
         elif cmd == 'halt':
             if len(sys.argv) == 3:
                 doShutdown(int(sys.argv[2]))
             else:
                 doShutdown()
+        elif cmd == 'cancel':
+            cancelShutdown()
+        else:
+            printHelp()
     else:
-        print('## raspiUps ## control script\nusage: piUps.py [ver] [batt] [onbatt] [time] [halt [t]]')
-        print('\tver\t\tshows UPS version in form of hw.fw')
-        print('\tbatt\t\tshows battery voltage [V]')
-        print('\tonbatt\t\tshows whether we run on battery (1) or on external power (0)')
-        print('\ttime\t\tshows run time on battery in [s] or 0 if powered from external source')
-        print('\thalt [t]\tinitiate UPS power off after an optionally defined timeout (default 30s)')
+        printHelp()
 
 
 if __name__ == "__main__":
